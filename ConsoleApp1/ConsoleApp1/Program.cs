9f49363f149454ec205630cf6a16ee39b2af1b63
twdeveloper <twDeveloper@hotmail.com>
2017-11-21 14:06:16 +0000

    加上 on exception

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,8 +15,8 @@ namespace ConsoleApp1
             //上面這行改為底下這樣
             BMIProcessor BMI = PolicyInjection.Create<BMIProcessor>();
             //其餘程式碼完全不變
-            BMI.Height = 170;
-            BMI.Weight = 70;
+            BMI.Height = 0         ;
+            BMI.Weight = 0;
 
             //計算BMI
             var ret = BMI.Calculate();
@@ -26,14 +26,39 @@ namespace ConsoleApp1
         }
     }
 
-    public class Logging : PolicyInjectionAttributeBase
+    public class ExceptionNotify : PolicyInjectionAttributeBase
+    {
+        //指定Log File Name
+        public string LogFileName { get; set; }
+        //override OnException方法
+        public override void OnException(object sender, PolicyInjectionAttributeEventArgs e)
+        {
+            var msg = $"\r\n exception({DateTime.Now.ToString()}) : \r\n{e.Exception.Message }";
+            //Console.Write(msg);
+            SaveLog(msg);
+        }
+
+        //寫入Log
+        private void SaveLog(string msg)
+        {
+            if (System.IO.File.Exists(LogFileName))
+            {
+                System.IO.File.AppendAllText(LogFileName, msg);
+            }
+            else
+            {
+                System.IO.File.WriteAllText(LogFileName, msg);
+            }
+        }
+    }
+        public class Logging : PolicyInjectionAttributeBase
     {
         //指定Log File Name
         public string LogFileName { get; set; }
         //override AfterInvoke方法
-        public override void AfterInvoke()
+        public override void AfterInvoke(object sender, PolicyInjectionAttributeEventArgs e)
         {
-            var msg = $"\r\n Method '{this.MethodBase.Name}' has been called - {DateTime.Now.ToString()} ";
+            var msg = $"\r\n Method '{e.MethodBase.Name}' has been called - {DateTime.Now.ToString()} ";
             SaveLog(msg);
         }
         //寫入Log
@@ -50,27 +75,28 @@ namespace ConsoleApp1
         }
     }
 
-public class BMIProcessor : PolicyInjectionComponentBase
-{
-    public int Weight { get; set; }
-    public int Height { get; set; }
-    public Decimal BMI
+    public class BMIProcessor : PolicyInjectionComponentBase
     {
-        get
+        public int Weight { get; set; }
+        public int Height { get; set; }
+        public Decimal BMI
         {
-            return Calculate();
+            get
+            {
+                return Calculate();
+            }
+        }
+
+        [ExceptionNotify(LogFileName = "log.txt")]
+        [Logging(LogFileName = "log.txt")]
+        //計算BMI
+        public Decimal Calculate()
+        {
+            Decimal result = 0;
+            Decimal height = (Decimal)Height / 100;
+            result = Weight / (height * height);
+
+            return result;
         }
     }
-
-    [Logging(LogFileName ="log.txt")]
-    //計算BMI
-    public Decimal Calculate()
-    {
-        Decimal result = 0;
-        Decimal height = (Decimal)Height / 100;
-        result = Weight / (height * height);
-
-        return result;
-    }
-}
 }
